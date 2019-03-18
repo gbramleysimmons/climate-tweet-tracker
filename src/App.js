9db8c6d2e0f44d30abedd111d94d8323f7fbc353
Esther Choi <ji_won_choi1@brown.edu>
2019-03-17 23:52:49 -0400

    added a rough format of a title--will be making more changes to the whole frame

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="wrapper">
+        <div className="wrapper-left">
+          <div className="title-block">
+            <div className="title-text">Climate #Hashtag Tracker</div>
+          </div>
           <Graph />
-        <HashtagContainer hashtags={["Lorem", "Ipsum", "Sin", "Dolor"]}/>
+          <HashtagContainer hashtags={["Lorem", "Ipsum", "Sin", "Dolor"]}/>
+        </div>
+        <div className="wrapper-right">
+          <div className="branding-block">
+            <div className="school">Brown University</div>
+            <div className="lab">Climate Development Lab</div>
+          </div>
+          <div className="Update"> {/* @Olivia: this div classNameshould be converted to a React Component */}
+          </div>
         </div>
       </div>
     );
