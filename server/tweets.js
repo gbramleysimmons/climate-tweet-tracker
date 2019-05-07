c3cc8cd3aa583c99541ef8a1c763ee6b48b83ef6
Grace Bramley-Simmons <gbramleysimmons@gmail.com>
2019-05-06 21:30:06 -0400

    fixed author issue

--- server/tweets.js
+++ server/tweets.js
@@ -302,6 +302,7 @@ class TweetRetriever {
             date: tweet.created_at,
             contents: tweet.text,
             hashtag: hashtag,
+            author: tweet.user,
             image: tweet.user.profile_image_url
         }
     }
