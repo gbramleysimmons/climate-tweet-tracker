12545d7ba4303760ec2c84cb239008c4bd848a18
Grace Bramley-Simmons <gbramleysimmons@gmail.com>
2019-05-07 10:29:25 -0400

    commiting the build directory lmao

---
+++ build/precache-manifest.b62643f106f1e1d2407a0d19c12498b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e41f3123ff652d46956e",
+    "url": "/static/css/main.77dbd1cf.chunk.css"
+  },
+  {
+    "revision": "e41f3123ff652d46956e",
+    "url": "/static/js/main.8cfa3b51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06be891cd95b203835eb",
+    "url": "/static/js/2.2c79fd45.chunk.js"
+  },
+  {
+    "revision": "97d399a8084d316c54e154b4d46d61dc",
+    "url": "/static/media/cdllogo2.97d399a8.png"
+  },
+  {
+    "revision": "4e31521a937e0b3e84307fe50f6d3d3b",
+    "url": "/index.html"
+  }
+];
